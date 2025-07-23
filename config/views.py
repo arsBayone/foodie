fd1b77992d81b6ca7481f08cfdca77d55bb97dff
arsBayone <sanjum@bayone.com>
2025-07-23 07:25:12 +0530

    Django learning

--- config/views.py
+++ config/views.py
@@ -2,10 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from random import choice
 
-fruits=["apple","banana","mangoes"]
+fruits={"name":"Arsalan", "profession":"SDE"}
 
 def index(request):
-    return render(request, "config/index.html")
+    context={"foods": fruits}
+    return render(request, "config/index.html",context  )
     # data={ "name":"Arsalan", "profession":"SDE"    }
     # data = choice(fruits)
     # return HttpResponse(f"Hello there {data}!")
