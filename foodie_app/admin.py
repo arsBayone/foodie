8f95dd865632f1e06a85eb6a283a3565045f69c1
arsBayone <sanjum@bayone.com>
2025-07-26 22:54:02 +0530

    Django learning

--- foodie_app/admin.py
+++ foodie_app/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from foodie_app.models import Category, Recipe
+
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id",'name', 'date_added')
+    search_fields = ['name']
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ("id", 'name', 'category', 'date_added')
+
+admin.site.register(Category, CategoryAdmin)    
+admin.site.register(Recipe, RecipeAdmin)
